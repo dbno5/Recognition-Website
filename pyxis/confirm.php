85b384e1474b568260c35aa47c17ea0b50ae3888
hernandv <hernandv@oregonstate.edu>
2017-03-15 14:12:21 -0500

    Login and Redirects
    Removed admin from $query and delayed redirects

--- pyxis/confirm.php
+++ pyxis/confirm.php
@@ -5,7 +5,8 @@ $passkey = $_GET['passkey'];
 $sql = "UPDATE Users SET com_code=NULL WHERE com_code='$passkey'";
 $result = mysqli_query($mysqli,$sql) or die(mysqli_error());
 if($result) {
-	echo '<div>Your account is now active. You may now <a href="login.php">Log in</a></div>';
+   echo "Your account is now active...redirecting to login.";
+   echo "<script>setTimeout(\"window.location.href = 'login.php';\",1500);</script>";
 }
 else {
 	echo "Some error occurred.";
