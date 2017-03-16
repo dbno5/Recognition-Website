9dd2bbd36ba26fee7ea250e2bd92f9afb4ac22c9
Nancy Chan <nancychannc@gmail.com>
2017-03-15 20:31:13 -0700

    Billy fixed the password issue

--- pyxis/users.php
+++ pyxis/users.php
@@ -1,9 +1,7 @@
 <?php
-$pageTitle = "Users";
-include("includes/backendHeader.php");
-?>
-
-    <?php
+    $pageTitle = "Users";
+    include("includes/backendHeader.php");   
+    
     $address = '"users.php"';
     ini_set('display_errors', 'On');
     $mysqli = new mysqli("oniddb.cws.oregonstate.edu","hernandv-db","J9RlSghRw6FKvLq8","hernandv-db");
@@ -46,9 +44,9 @@ include("includes/backendHeader.php");
     ?>
         <link rel="stylesheet" href="CSS/form_stylesheet.css" />
         <script src="DeleteButton.js"></script>
-        <script src="Navigation.js"></script>
+       <!-- <script src="Navigation.js"></script>-->
         <div class="form_container">
-            <h3>User Form</h3>
+            <h3>User Information</h3>
             <form id="input_form" action="users.php" method="post">
                 <?php
                 if(empty($_GET) == false && empty($_GET['edit']) == false)
@@ -62,7 +60,7 @@ include("includes/backendHeader.php");
                     if(!$stmt->execute()){
                         echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
                     }
-                    if(!$stmt->bind_result($email, $fname, $lname, $username, $password, $signature, $jobtitle, $userstatus))
+                    if(!$stmt->bind_result($email, $fname, $lname, $username, $userpassword, $location, $jobtitle, $userstatus))
                     {
                         echo "Bind failed: " . $mysqli->connect_errno . " " . $mysqli->connect_error;
                     }
@@ -76,9 +74,9 @@ include("includes/backendHeader.php");
                     echo "<label class='form_label'>Username</label>\n";
                     echo "<input maxlength='255' name='Username' type='text' value='" . $username . "'/>\n";
                     echo "<label class='form_label'>UserPassword</label>\n";
-                    echo "<input maxlength='255' name='Password' type='text' value='" . $password . "'/>\n";
+                    echo "<input maxlength='255' name='UserPassword' type='text' value='" . $userpassword . "'/>\n";
                     echo "<label class='form_label'>Location</label>\n";
-                    echo "<input maxlength='255' name='Location' type='text' value='" . $signature . "'/>\n";
+                    echo "<input maxlength='255' name='Location' type='text' value='" . $location . "'/>\n";
                     echo "<label class='form_label'>Job Title</label>\n";
                     echo "<input maxlength='255' name='JobTitle' type='text' value='" . $jobtitle . "'/>\n";
                     echo "<label class='form_label'>UserStatus</label>\n";
@@ -97,7 +95,7 @@ include("includes/backendHeader.php");
                     echo    "<input name='Email' type='text' />\n";
                     echo    "<label class='form_label'>Username</label>\n";
                     echo    "<input name='Username' type='text' />\n";
-                    echo    "<label class='form_label'>Password</label>\n";
+                    echo    "<label class='form_label'>UserPassword</label>\n";
                     echo    "<input name='UserPassword' type='text' />\n";
                     echo    "<label class='form_label'>Location</label>\n";
                     echo    "<input name='Location' type='text' />\n";
@@ -114,7 +112,6 @@ include("includes/backendHeader.php");
         </div>
 
 
-    <!-- Patient selection -->
     <?php
     if((empty($_POST) == true && empty($_POST['edit']) == false) || empty($_GET['edit']) == true)
     {
@@ -126,7 +123,7 @@ include("includes/backendHeader.php");
                 <th>ID</th>
                 <th>First Name</th>
                 <th>Last Name</th>
-                <th>Email</th>>
+                <th>Email</th>
                 <th>Username</th>
                 <th>Password</th>
                 <th>Location</th>      
@@ -141,14 +138,14 @@ include("includes/backendHeader.php");
         {
             echo "Execute failed: " . $mysqli->connect_errno . " " . $mysqli->connect_error;
         }
-        if(!$stmt->bind_result($id, $email, $fname, $lname, $username, $password, $signature, $jobtitle, $userstatus))
+        if(!$stmt->bind_result($id, $email, $fname, $lname, $username, $userpassword, $location, $jobtitle, $userstatus))
         {
             echo "Bind failed: " . $mysqli->connect_errno . " " . $mysqli->connect_error;
         }
         $counter = 0;
         while($stmt->fetch())
         {
-            echo "<tr id='" . $id . "'>\n<td>\n" . $id . "</td>\n<td>\n" . $fname . "</td>\n<td>\n" . $lname .  "</td>\n<td>\n" . $email ."</td>\n<td>\n"  . $username . "</td>\n <td>\n" . $password . "</td>\n <td>\n" . $signature . "</td>\n <td>\n" . $jobtitle . "</td>\n <td>\n" . $userstatus . "</td>\n";
+            echo "<tr id='" . $id . "'>\n<td>\n" . $id . "</td>\n<td>\n" . $fname . "</td>\n<td>\n" . $lname .  "</td>\n<td>\n" . $email ."</td>\n<td>\n"  . $username . "</td>\n <td>\n" . $userpassword . "</td>\n <td>\n" . $location . "</td>\n <td>\n" . $jobtitle . "</td>\n <td>\n" . $userstatus . "</td>\n";
     /*        echo "<td>\n<input class='btn btn-default'onclick='GotoPatientGenes(" . $id . ")' type='button'  value='Genes'/></td>\n";
             echo "<td>\n<input class='btn btn-secondary'onclick='GotoPatientDiseases(" . $id . ")' type='button'  value='Diseases'/></td>\n";
             echo "<td>\n<input class='btn btn-info' onclick='GotoPatientDrugs(" . $id . ")' type='button'  value='Drugs'/></td>\n";
